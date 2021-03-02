3bfa961c1fc8bee8e3ef90bbb139680e9126a28f
metermaid <718216+metermaid@users.noreply.github.com>
2021-03-01 23:52:50 -0500

    Add sample user settings

---
+++ user-config.sample.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# This is a sample file. You should use generate_user_files.py
+# to create your user-config.py file.
+
+family_files['blaseball'] = 'https://www.blaseball.wiki/api.php'
+family = 'blaseball'
+mylang = 'blaseball'  # a workaround for this bug: https://phabricator.wikimedia.org/T130570
+password_file = 'user-passwords'
+
+usernames['blaseball']['blaseball'] = 'OliverIsARobot'
